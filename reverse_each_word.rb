095c7d46b03dca4516dfab4753b14f6682e15e8e
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-22 10:50:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,13 @@
-def reverse_each_word(string)
+def reverse_each_word_with_each(string)
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
+
+def reverse_each_word_collect(string)
   array = []
   string.split(" ").collect do |string|
     array.push(string.reverse)
